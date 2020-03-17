b0f3360858d7482c7a71b736455b4412b1302e69
Cole Trumbo <cole.trumbo@gmail.com>
2020-03-16 20:18:33 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # sql-librarian
+
+Group project with Cole Trumbo, Rory Burke, Daniel Barone, and Kevin Lai.
+
+
+The SQL procedures I wrote:
+admin_branches,
+check_card_numer_exists,
+get_books_due
+
+Plus a procedure to get the books available for checkout at a given branch (checking that noOfCopies is greater than book loaned out.) This was replaced with other proceedures along the way.
+
+I also helped with the Borrower class in python.
+
+I ran through the program testing various cases, and researched unittest and unittest.mock. Since nobody on the team had experience with it, the team felt it was simpler to test manually, as the program is small.
---
+++ tests/test_borrower.py
@@ -0,0 +1,30 @@
+from unittest.mock import patch
+from unittest import TestCase
+import unittest
+import borrower
+
+def get_input(text):
+    return input(text)
+
+
+def answer():
+    ans = get_input('enter yes or no')
+    if ans == 'yes':
+        return 'you entered yes'
+    if ans == 'no':
+        return 'you entered no'
+
+
+class TestBorrower(TestCase):
+
+    # get_input will return 'yes' during this test
+    @patch('get_input', return_value='yes')
+    def test_answer_yes(self, input):
+        self.assertEqual(answer(), 'you entered yes')
+
+    @patch('get_input', return_value='no')
+    def test_answer_no(self, input):
+        self.assertEqual(answer(), 'you entered no')
+
+if __name__ == '__main__':
+    unittest.main()
